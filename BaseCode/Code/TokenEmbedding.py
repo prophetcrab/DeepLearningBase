5bf28ed51416c5f102be2022a21c4d44fc60f91f
prophetcrab <35569881+prophetcrab@users.noreply.github.com>
2024-07-02 22:04:20 +0900

    Delete BaseCode/Code directory

--- BaseCode/Code/TokenEmbedding.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
-
-class TokenEmbedding(nn.Module):
-    def __init__(self, vocab_size, d_model):
-        super(TokenEmbedding, self).__init__(vocab_size, d_model, padding_idx=1)
